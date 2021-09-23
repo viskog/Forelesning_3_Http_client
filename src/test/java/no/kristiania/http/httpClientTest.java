6124449644c2038ec08267ce98faba7fe38c61ff
Vigleik <visk010@student.kristiania.no>
2021-09-23 14:49:21 +0200

    Add initial test

---
+++ src/test/java/no/kristiania/http/httpClientTest.java
@@ -0,0 +1,14 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class httpClientTest {
+
+    @Test
+    void dummyTest() {
+        assertEquals(42, 6*7);
+    }
+
+}
