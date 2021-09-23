ba099e5cdec8d9140c87820594e78b270e263991
Vigleik <visk010@student.kristiania.no>
2021-09-23 16:05:53 +0200

    test shouldReturn404StatusCode (refactored)

--- src/main/java/no/kristiania/http/httpClient.java
+++ src/main/java/no/kristiania/http/httpClient.java
@@ -18,14 +18,20 @@ public class httpClient {
                         "\r\n").getBytes()
         );
 
+        String statusLine = readLine(socket);
+        this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
+    }
+
+    private String readLine(Socket socket) throws IOException {
         StringBuilder result = new StringBuilder();
         InputStream in = socket.getInputStream();
+
         int c;
-        while ((c = in.read()) != -1) {
+        while ((c = in.read()) != -1 && c != '\r') {
             result.append((char)c);
         }
-        String responseMessage = result.toString();
-        this.statusCode = Integer.parseInt(responseMessage.split(" ")[1]);
+
+        return result.toString();
     }
 
     public int getStatusCode() {
